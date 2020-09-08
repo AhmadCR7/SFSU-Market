e6ce6cd545f3d484e22526a6ca50103a9eb0af60
AhmadCR7 <aafghan@mail.sfsu.edu>
2020-09-08 12:32:28 -0700

    about me

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import AhmadApp from "./component/Ahmad";
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+const App = () => {
+  console.log("Rendering App");
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World 🎉
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div class="App">
+        <div>
+          <div>
+            <Link to="/home" class="button">
+              HomePage
+            </Link>
+          </div>
+
+          <div>
+            <div>
+              <Link to="/ahmadcr7" class="button">
+                Ahmad
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <Switch>
+          <Route path="/ahmadcr7" component={AhmadApp} />
+          <Route path="/home"></Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ application/client/src/component/Ahmad.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AhmadApp = () => {
+  return <p>About me AHMAD</p>;
+};
+
+export default AhmadApp;
