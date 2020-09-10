83cbd7152141638906f6aad921d371e2dbc26e71
Steven McHenry <stevenmchenry01@gmail.com>
2020-09-10 15:39:33 -0700

    moved ahmad compnent into folder and added my page

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,59 +1,60 @@
-import React from "react";
-import AhmadApp from "./component/Ahmad";
-import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
-import Homepage from "./component/Homepage";
+import React from 'react'
+import AhmadApp from './component/Ahmad/Ahmad'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+import Homepage from './component/Homepage'
+import StevenPage from './component/Steven/StevenPage'
 
 const App = () => {
-  console.log("Rendering App");
+  console.log('Rendering App')
 
   return (
     <BrowserRouter>
-      <div class="App">
+      <div class='App'>
         <div>
           <div>
-            <Link to="/home" class="button">
+            <Link to='/home' class='button'>
               HomePage
             </Link>
           </div>
 
           <div>
-            <Link to="/ahmadcr7" class="button">
+            <Link to='/ahmadcr7' class='button'>
               Ahmad
             </Link>
           </div>
           <div>
-            <Link to="/nickgreensf" class="button">
+            <Link to='/nickgreensf' class='button'>
               Nicholas
             </Link>
           </div>
           <div>
-            <Link to="/laurenlukee" class="button">
+            <Link to='/laurenlukee' class='button'>
               Lauren
             </Link>
           </div>
           <div>
-            <Link to="/stevenmchenry01" class="button">
+            <Link to='/stevenmchenry01' class='button'>
               Steven
             </Link>
           </div>
           <div>
-            <Link to="/WaqasHassan7" class="button">
+            <Link to='/WaqasHassan7' class='button'>
               Waqas
             </Link>
           </div>
         </div>
 
         <Switch>
-          <Route path="/home" component={Homepage}></Route>
-          <Route path="/ahmadcr7" component={AhmadApp} />
-          <Route path="/nickgreensf" />
-          <Route path="/laurenlukee" />
-          <Route path="/stevenmchenry01" />
-          <Route path="//WaqasHassan7" />
+          <Route path='/home' component={Homepage}></Route>
+          <Route path='/ahmadcr7' component={AhmadApp} />
+          <Route path='/nickgreensf' />
+          <Route path='/laurenlukee' />
+          <Route path='/stevenmchenry01' component={StevenPage} />
+          <Route path='//WaqasHassan7' />
         </Switch>
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-export default App;
+export default App
