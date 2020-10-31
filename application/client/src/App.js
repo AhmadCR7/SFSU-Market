2da38c3b61fc4e772ba41b93ee914a3a60a8d252
Steven McHenry <stevenmchenry01@gmail.com>
2020-10-31 16:58:38 -0700

    fixed linting

--- application/client/src/App.js
+++ application/client/src/App.js
@@ -1,30 +1,29 @@
-import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Aboutuspage from "./pages/Aboutus/Aboutus";
-import Navbar from "./component/Navbar/Navbar.js"
-import Homepage from "./pages/Homepage/Homepage"
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Aboutuspage from './pages/Aboutus/Aboutus'
+import Navbar from './component/Navbar/Navbar'
+import Homepage from './pages/Homepage/Homepage'
 import Login from './pages/Login/Login'
-import Signup from "./pages/Signup/Signup";
-import Listings from "./pages/Lisitngs/Listings";
+import Signup from './pages/Signup/Signup'
+import Listings from './pages/Lisitngs/Listings'
 
 function App() {
-  console.log("Rendering App");
+  console.log('Rendering App')
 
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <Navbar />
         <Switch>
-          <Route path="/" exact component={Homepage} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/aboutus" component={Aboutuspage} />
-          <Route path="/signup" exact component={Signup} />
-          <Route path="/listings" component={Listings}/>
+          <Route path='/' exact component={Homepage} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/aboutus' component={Aboutuspage} />
+          <Route path='/signup' exact component={Signup} />
+          <Route path='/listings' component={Listings} />
         </Switch>
       </div>
     </BrowserRouter>
-  );
-};
+  )
+}
 
-
-export default App;
+export default App
